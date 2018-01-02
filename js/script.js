55ab7a6ca1c9c9e2c7c79695ee655762a877f278
Dzuev76 <Dzuev76@gmail.com>
2018-01-02 14:41:37 +0600

    change

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
- $(document).ready(function(){
+$(document).ready(function(){
         $("#anchor-link-1").on("click", function(){
             var scroll=$("#services").offset().top;
             $('html, body').animate({scrollTop: scroll}, 2000);
@@ -8,11 +8,11 @@
             $('html, body').animate({scrollTop: scroll}, 2000);
         });
         $("#anchor-link-3").on("click", function(){
-            var scroll=$("#anchor-3").offset().top;
+            var scroll=$("#about").offset().top;
             $('html, body').animate({scrollTop: scroll}, 2000);
         });
         $("#anchor-link-4").on("click", function(){
-            var scroll=$("#anchor-4").offset().top;
+            var scroll=$("#contact").offset().top;
             $('html, body').animate({scrollTop: scroll}, 2000);
         });
     });
